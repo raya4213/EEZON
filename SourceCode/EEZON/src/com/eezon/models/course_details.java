9929ed42d156c53a02e950b7e35286012c183a5b
Chinmay <Chinmay@10.201.67.74>
2017-04-25 21:42:31 -0600

    Commit for model for course details and hibernate file

---
+++ SourceCode/EEZON/src/com/eezon/models/course_details.java
@@ -0,0 +1,44 @@
+package com.eezon.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class course_details {
+	@Id
+	private String courseName;
+	private String year;
+	private	String semester;
+	private String description;
+	
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+		
+	
+	
+	
+	
+}

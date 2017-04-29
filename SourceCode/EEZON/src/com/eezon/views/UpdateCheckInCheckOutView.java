bd0e44c77f5f0328f5cf549e41c29f46cd85910a
raya4213 <raya4213@colorado.edu>
2017-04-29 14:47:57 -0600

    Updating view for UpdateCheckInCheckOutView.java All the variables from open are taken outside

--- SourceCode/EEZON/src/com/eezon/views/UpdateCheckInCheckOutView.java
+++ SourceCode/EEZON/src/com/eezon/views/UpdateCheckInCheckOutView.java
@@ -12,35 +12,47 @@ import org.eclipse.swt.widgets.Group;
 public class UpdateCheckInCheckOutView {
 	private Text txtEmail;
 	private Table tblCinCoutDetails;
+	Button btnHome;
+	Button btnBack;
+	Group group;
+	Button btnCheckIn;
+	Button btnCheckOut;
+	Combo cmbSelectCourse;
+	Combo cmbSelectKitType;
+	Combo cmbKitSerialNumber;
+	Button btnCheckOut_1;
+	Button btnCheckIn_1;
+	Display display;
+	Shell shlEezonCheck;
 
 	/**
 	 * Launch the application.
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		try {
-			UpdateCheckInCheckOutView window = new UpdateCheckInCheckOutView();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public static void main(String[] args) {
+//		try {
+//			UpdateCheckInCheckOutView window = new UpdateCheckInCheckOutView();
+//			window.open();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	/**
 	 * Open the window.
 	 */
 	public void open() {
-		Display display = Display.getDefault();
-		Shell shlEezonCheck = new Shell();
+		display = Display.getDefault();
+		shlEezonCheck = new Shell();
 		shlEezonCheck.setSize(1132, 684);
 		shlEezonCheck.setText("EEZON - Check IN/Check OUT");
 		
-		Button btnHome = new Button(shlEezonCheck, SWT.NONE);
+		btnHome = new Button(shlEezonCheck, SWT.NONE);
 		btnHome.setBounds(44, 27, 105, 35);
 		btnHome.setText("HOME");
 		
-		Button btnBack = new Button(shlEezonCheck, SWT.NONE);
-		btnBack.setBounds(186, 27, 105, 35);
+		btnBack = new Button(shlEezonCheck, SWT.NONE);
+		btnBack.setBounds(943, 27, 105, 35);
 		btnBack.setText("BACK");
 		
 		txtEmail = new Text(shlEezonCheck, SWT.BORDER);
@@ -48,28 +60,28 @@ public class UpdateCheckInCheckOutView {
 		txtEmail.setToolTipText("Enter Email ID");
 		txtEmail.setBounds(443, 94, 162, 31);
 		
-		Group group = new Group(shlEezonCheck, SWT.NONE);
+		group = new Group(shlEezonCheck, SWT.NONE);
 		group.setBounds(456, 131, 149, 81);
 		
-		Button btnCheckIn = new Button(group, SWT.RADIO);
+		btnCheckIn = new Button(group, SWT.RADIO);
 		btnCheckIn.setBounds(10, 23, 133, 25);
 		btnCheckIn.setText("Check IN");
 		
-		Button btnCheckOut = new Button(group, SWT.RADIO);
+		btnCheckOut = new Button(group, SWT.RADIO);
 		btnCheckOut.setBounds(10, 54, 133, 25);
 		btnCheckOut.setText("Check OUT");
 		
-		Combo cmbSelectCourse = new Combo(shlEezonCheck, SWT.NONE);
+		cmbSelectCourse = new Combo(shlEezonCheck, SWT.NONE);
 		cmbSelectCourse.setItems(new String[] {"Embedded System Design", "Computer Vision", "Real Time Embedded Systems", "Computer Architecture", "Programmable System on Chip", "Internet of Things", "Low Power Embedded Design Techniques"});
 		cmbSelectCourse.setBounds(470, 235, 104, 33);
 		
-		Combo cmbSelectKitType = new Combo(shlEezonCheck, SWT.NONE);
+		cmbSelectKitType = new Combo(shlEezonCheck, SWT.NONE);
 		cmbSelectKitType.setBounds(470, 284, 104, 33);
 		
-		Combo cmbKitSerialNumber = new Combo(shlEezonCheck, SWT.NONE);
+		cmbKitSerialNumber = new Combo(shlEezonCheck, SWT.NONE);
 		cmbKitSerialNumber.setBounds(470, 338, 104, 33);
 		
-		Button btnCheckOut_1 = new Button(shlEezonCheck, SWT.NONE);
+		btnCheckOut_1 = new Button(shlEezonCheck, SWT.NONE);
 		btnCheckOut_1.setBounds(469, 396, 105, 35);
 		btnCheckOut_1.setText("Check OUT");
 		
@@ -78,7 +90,7 @@ public class UpdateCheckInCheckOutView {
 		tblCinCoutDetails.setHeaderVisible(true);
 		tblCinCoutDetails.setLinesVisible(true);
 		
-		Button btnCheckIn_1 = new Button(shlEezonCheck, SWT.NONE);
+		btnCheckIn_1 = new Button(shlEezonCheck, SWT.NONE);
 		btnCheckIn_1.setBounds(469, 521, 105, 35);
 		btnCheckIn_1.setText("Check IN");
 

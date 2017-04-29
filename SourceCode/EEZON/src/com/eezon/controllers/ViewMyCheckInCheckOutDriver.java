ad6412664f15432082c4f349d164b0bad0dff3db
raya4213 <raya4213@colorado.edu>
2017-04-29 14:26:03 -0600

    Update

--- SourceCode/EEZON/src/com/eezon/controllers/ViewMyCheckInCheckOutDriver.java
+++ SourceCode/EEZON/src/com/eezon/controllers/ViewMyCheckInCheckOutDriver.java
@@ -4,5 +4,7 @@ public class ViewMyCheckInCheckOutDriver {
 	public static void main(String args[]){
 		ViewMyCheckInCheckOutController viewMyCheckInCheckOutController = new ViewMyCheckInCheckOutController();
 		viewMyCheckInCheckOutController.displayView();
+		System.out.println("In view check in check out controller");
+		
 	}
 }

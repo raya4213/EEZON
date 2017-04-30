a29627fcae5c2e62d5b7be22b092ddb5094990ec
sharathvontari <sharath.vontari@colorado.edu>
2017-04-29 22:05:59 -0600

    adding getter setter methods

--- SourceCode/EEZON/src/com/eezon/controllers/UnavailableItemReqController.java
+++ SourceCode/EEZON/src/com/eezon/controllers/UnavailableItemReqController.java
@@ -31,6 +31,23 @@ public class UnavailableItemReqController {
 		}
 	}
 	
-	
+	public UnavailableItemReqView getUnavailableItemReqView() {
+		return unavailableItemReqView;
+	}
+	public void setUnavailableItemReqView(UnavailableItemReqView unavailableItemReqView) {
+		this.unavailableItemReqView = unavailableItemReqView;
+	}
+	public Course getCourseModel() {
+		return courseModel;
+	}
+	public void setCourseModel(Course courseModel) {
+		this.courseModel = courseModel;
+	}
+	public UnavailableItemRequest getUnavailableItemReqModel() {
+		return unavailableItemReqModel;
+	}
+	public void setUnavailableItemReqModel(UnavailableItemRequest unavailableItemReqModel) {
+		this.unavailableItemReqModel = unavailableItemReqModel;
+	}
 	 
 }

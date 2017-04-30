a4bb2d315ce6570f3af7f781688bc27e5f71d1db
sharathvontari <sharath.vontari@colorado.edu>
2017-04-29 22:23:38 -0600

    code clean

--- SourceCode/EEZON/src/com/eezon/controllers/CheckInExtensionDriver.java
+++ SourceCode/EEZON/src/com/eezon/controllers/CheckInExtensionDriver.java
@@ -5,8 +5,7 @@ public static void main(String args[]){
 		
 		CheckInExtensionReqController checkInExtensionReqController= new CheckInExtensionReqController();
 		checkInExtensionReqController.displayView();
-		//penaltyMgtController.getPenaltyView().open();
-		//penaltyMgtController.in
+		
 	}
 
 }

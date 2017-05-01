7a9eb3296adc7b0c586e855be37e78e70376c66c
Sairam Udaya Janardhana Muttavarapu <sairam.muttavarapu@gmail.com>
2017-05-01 02:31:55 -0600

    Adding Confirmation Dialog in PenaltyOverride Button onclick..

--- SourceCode/EEZON/src/com/eezon/penalty/strategy/BtnOverrideAction.java
+++ SourceCode/EEZON/src/com/eezon/penalty/strategy/BtnOverrideAction.java
@@ -1,6 +1,9 @@
 package com.eezon.penalty.strategy;
 
+import org.eclipse.swt.SWT;
+
 import com.eezon.controllers.PenaltyManagementController;
+import com.eezon.dialogs.ConfirmationDialog;
 import com.eezon.models.Kit;
 import com.eezon.views.PenaltyManagementView;
 
@@ -11,17 +14,21 @@ public class BtnOverrideAction implements IPenaltyBtnAction {
 		// TODO Auto-generated method stub
 		System.out.println("Override Btn pressed");
 		System.out.println("NumSelected"+penaltyView.getTblViewPenalties().getSelectionCount());
-		int selectedKitIndices [] = penaltyView.getTblViewPenalties().getSelectionIndices();
-		for(int selectedKitIndex : selectedKitIndices){
-			System.out.println("SerialNum"+penaltyView.getTblViewPenalties().getItem(selectedKitIndex));
-			String serialNum = penaltyView.getTblViewPenalties().getItem(selectedKitIndex).getText();
-			System.out.println(serialNum);
-			Kit kitSelected = kitModel.getKitDetailsForSerialNum(serialNum);
-			kitSelected.setKitPenalty(0.0);
-			kitModel.updateKitDetails(kitSelected);
-			
-		}
 		
+		ConfirmationDialog confirmDialog = new ConfirmationDialog(penaltyView.getShlPenaltyManagement(), SWT.NULL);
+		String result = (String)confirmDialog.open();
+		
+		if(result.equalsIgnoreCase("Yes")){
+			int selectedKitIndices [] = penaltyView.getTblViewPenalties().getSelectionIndices();
+			for(int selectedKitIndex : selectedKitIndices){
+				System.out.println("SerialNum"+penaltyView.getTblViewPenalties().getItem(selectedKitIndex));
+				String serialNum = penaltyView.getTblViewPenalties().getItem(selectedKitIndex).getText();
+				System.out.println(serialNum);
+				Kit kitSelected = kitModel.getKitDetailsForSerialNum(serialNum);
+				kitSelected.setKitPenalty(0.0);
+				kitModel.updateKitDetails(kitSelected);
+			}
+		}
 	}
 
 }

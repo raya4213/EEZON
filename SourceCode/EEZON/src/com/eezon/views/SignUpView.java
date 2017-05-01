61771db798f693a52100712cd98ffd0959f3d819
Rahul Yamasani <raya4213@colorado.edu>
2017-05-01 03:21:40 -0600

    Adding necessary changes to support MVC

--- SourceCode/EEZON/src/com/eezon/views/SignUpView.java
+++ SourceCode/EEZON/src/com/eezon/views/SignUpView.java
@@ -16,26 +16,32 @@ public class SignUpView {
 	private Text txtEnterAccessCode;
 	private Text txtEnterPassword_1;
 	private Text txtConfirmPassword;
+	Combo combo;
+	Display display;
+	Shell shlSignUp;
+	Button btnSignUp;
+	Display display_1;
+	Button btnDisplay;
 
 	/**
 	 * Launch the application.
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		try {
-			SignUpView window = new SignUpView();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public static void main(String[] args) {
+//		try {
+//			SignUpView window = new SignUpView();
+//			window.open();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	/**
 	 * Open the window.
 	 */
-	public void open() {
-		Display display = Display.getDefault();
-		Shell shlSignUp = new Shell();
+	public SignUpView() {
+		display = Display.getDefault();
+		shlSignUp = new Shell();
 		shlSignUp.setSize(1214, 698);
 		shlSignUp.setText("Sign Up");
 		
@@ -47,7 +53,7 @@ public class SignUpView {
 		txtEnterPassword.setText("Enter Password");
 		txtEnterPassword.setBounds(483, 142, 159, 31);
 		
-		Combo combo = new Combo(shlSignUp, SWT.NONE);
+		combo = new Combo(shlSignUp, SWT.NONE);
 		combo.setItems(new String[] {"Professor", "Student"});
 		combo.setBounds(517, 243, 104, 33);
 		combo.setText("Role");
@@ -68,7 +74,7 @@ public class SignUpView {
 		txtConfirmPassword.setText("Confirm Password");
 		txtConfirmPassword.setBounds(527, 502, 173, 31);
 		
-		Button btnSignUp = new Button(shlSignUp, SWT.NONE);
+		btnSignUp = new Button(shlSignUp, SWT.NONE);
 		btnSignUp.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -85,4 +91,126 @@ public class SignUpView {
 			}
 		}
 	}
+
+	public Text getTxtEnterEmailId() {
+		return txtEnterEmailId;
+	}
+ 
+	
+	public void setTxtEnterEmailId(Text txtEnterEmailId) {
+		this.txtEnterEmailId = txtEnterEmailId;
+	}
+
+	
+	public Text getTxtEnterPassword() {
+		return txtEnterPassword;
+	}
+
+	
+	public void setTxtEnterPassword(Text txtEnterPassword) {
+		this.txtEnterPassword = txtEnterPassword;
+	}
+
+	
+	public Text getTxtAccessCodeMessage() {
+		return txtAccessCodeMessage;
+	}
+
+	
+	public void setTxtAccessCodeMessage(Text txtAccessCodeMessage) {
+		this.txtAccessCodeMessage = txtAccessCodeMessage;
+	}
+
+	
+	public Text getTxtEnterAccessCode() {
+		return txtEnterAccessCode;
+	}
+
+	
+	public void setTxtEnterAccessCode(Text txtEnterAccessCode) {
+		this.txtEnterAccessCode = txtEnterAccessCode;
+	}
+
+	
+	public Text getTxtEnterPassword_1() {
+		return txtEnterPassword_1;
+	}
+
+	
+	public void setTxtEnterPassword_1(Text txtEnterPassword_1) {
+		this.txtEnterPassword_1 = txtEnterPassword_1;
+	}
+
+	
+	public Text getTxtConfirmPassword() {
+		return txtConfirmPassword;
+	}
+
+	
+	public void setTxtConfirmPassword(Text txtConfirmPassword) {
+		this.txtConfirmPassword = txtConfirmPassword;
+	}
+
+	
+	public Combo getCombo() {
+		return combo;
+	}
+
+	
+	public void setCombo(Combo combo) {
+		this.combo = combo;
+	}
+
+	
+	public Display getDisplay() {
+		return display;
+	}
+
+	
+	public void setDisplay(Display display) {
+		this.display = display;
+	}
+
+	
+	public Shell getShlSignUp() {
+		return shlSignUp;
+	}
+
+	
+	public void setShlSignUp(Shell shlSignUp) {
+		this.shlSignUp = shlSignUp;
+	}
+
+	
+	public Button getBtnSignUp() {
+		return btnSignUp;
+	}
+
+	
+	public void setBtnSignUp(Button btnSignUp) {
+		this.btnSignUp = btnSignUp;
+	}
+
+	
+	public Display getDisplay_1() {
+		return display_1;
+	}
+
+	
+	public void setDisplay_1(Display display_1) {
+		this.display_1 = display_1;
+	}
+
+	
+	public Button getBtnDisplay() {
+		return btnDisplay;
+	}
+
+	
+	public void setBtnDisplay(Button btnDisplay) {
+		this.btnDisplay = btnDisplay;
+	}
+	
+	
+	
 }
